c3fe6e5464fbc520b61e43ab1959bced7770c749
Rasha ali <rashaali84@gmail.com>
2020-03-08 20:34:06 +0100

    save number

--- handlers/save.js
+++ handlers/save.js
@@ -1,11 +1,23 @@
 function saveHandler() {
   debugger;
   // write me!
+  let inputValue = document.getElementById("input").value;
+  const num = Number(parseInt(inputValue));
+  var numbers = /^[0-9]+$/;
+  if (!inputValue.match(numbers)) {
+    //it is not a number
+    strings.NaNy.push(inputValue);
+  }
+  else if (num % 2 == 0) //even 
+  {
+    strings.evens.push(inputValue);
+  }
+  else { strings.odds.push(inputValue); }
 
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'save',
-    newInput,
+    inputValue,
     strings: JSON.parse(JSON.stringify(strings))
   })
 }
---
+++ listeners/save.js
@@ -0,0 +1,2 @@
+// attach a click event listener to trigger resetting the project
+document.getElementById("save-button").addEventListener("click", saveHandler);

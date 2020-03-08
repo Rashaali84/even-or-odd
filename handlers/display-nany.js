7f4cf237b30485d69a1bdafe03a334fa95fd5100
Rasha ali <rashaali84@gmail.com>
2020-03-08 20:39:46 +0100

    display nany

--- handlers/display-nany.js
+++ handlers/display-nany.js
@@ -1,6 +1,22 @@
 function nanyHandler() {
   debugger;
-  // write me!
+  // solution 2
+  /*const ulE = document.getElementById("ordered_List");
+  ulE.innerHTML = "";
+  var i;
+  for (i = 0; i < strings.NaNy.length; i++) {
+    const li_1 = document.createElement('li');
+    li_1.innerText = textIn;
+    ulE.appendChild(li_1);
+  }*/
+  //solution 1
+  strings.displayed = "NaNy";
+  const currentDisplayed = strings.displayed;
+  const currentArray = strings[currentDisplayed];
+  const currentListComponent = renderList(currentArray, currentDisplayed);
+  const listContainer = document.getElementById('selected');
+
+  listContainer.appendChild(currentListComponent);
 
   // log user interaction: handler name, user input, new state
   log.push({
---
+++ listeners/display-nany.js
@@ -0,0 +1,2 @@
+// attach a click event listener to trigger resetting the project
+document.getElementById("nany-button").addEventListener("click", nanyHandler);

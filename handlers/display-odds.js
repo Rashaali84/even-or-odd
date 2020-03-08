7a3b119978221a5f58671ef1908efa74dbbd121a
Rasha ali <rashaali84@gmail.com>
2020-03-08 20:38:28 +0100

    display odd

--- handlers/display-odds.js
+++ handlers/display-odds.js
@@ -1,6 +1,21 @@
 function oddsHandler() {
   debugger;
-  // write me!
+  // solution 2
+  /*const ulE = document.getElementById("ordered_List");
+  ulE.innerHTML = "";
+  var i;
+  for (i = 0; i < strings.odds.length; i++) {
+    const li_1 = document.createElement('li');
+    li_1.innerText = textIn;
+    ulE.appendChild(li_1);
+  }*/
+  strings.displayed = "odds";
+  const currentDisplayed = strings.displayed;
+  const currentArray = strings[currentDisplayed];
+  const currentListComponent = renderList(currentArray, currentDisplayed);
+  const listContainer = document.getElementById('selected');
+
+  listContainer.appendChild(currentListComponent);
 
   // log user interaction: handler name, user input, new state
   log.push({
---
+++ listeners/display-odds.js
@@ -0,0 +1,2 @@
+// attach a click event listener to trigger resetting the project
+document.getElementById("odds-button").addEventListener("click", oddsHandler);

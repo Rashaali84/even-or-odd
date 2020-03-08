1cddd7f00ab021b2dfc1e60531f0b1e7666cf0a5
Rasha ali <rashaali84@gmail.com>
2020-03-08 20:35:57 +0100

    display even

--- handlers/display-evens.js
+++ handlers/display-evens.js
@@ -1,6 +1,21 @@
 function evensHandler() {
   debugger;
-  // write me!
+  // this is my dom solution --- 1
+  /* const ulE = document.getElementById("ordered_List");
+   ulE.innerHTML = "";
+   var i;
+   for (i = 0; i < strings.evens.length; i++) {
+   const li_1 = document.createElement('li');
+   li_1.innerText = textIn;
+   ulE.appendChild(li_1);*/
+
+  strings.displayed = "evens";
+  const currentDisplayed = strings.displayed;
+  const currentArray = strings[currentDisplayed];
+  const currentListComponent = renderList(currentArray, currentDisplayed);
+  const listContainer = document.getElementById('selected');
+
+  listContainer.appendChild(currentListComponent);
 
   // log user interaction: handler name, user input, new state
   log.push({
---
+++ listeners/display-evens.js
@@ -0,0 +1,2 @@
+// attach a click event listener to trigger resetting the project
+document.getElementById("evens-button").addEventListener("click", evensHandler);
